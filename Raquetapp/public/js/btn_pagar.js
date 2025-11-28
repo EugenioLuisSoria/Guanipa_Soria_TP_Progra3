be321797033a53fe6bb97ae5e9f2df2502f454af
Eugenio Soria <eugeniosoria@gmail.com>
2025-11-28 11:05:32 -0300

    Css Carrito: ocultar btnPago

--- Raquetapp/public/js/btn_pagar.js
+++ Raquetapp/public/js/btn_pagar.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const formPagar = document.querySelector(".formPagar");
+    const btnPagar = document.querySelector(".btnPagar");
     const modal = document.getElementById("modal_pagar");
     const btnConfirmar = modal.querySelector(".btn_confirmar_modal");
     const btnCancelar = modal.querySelector(".btn_cerrar_modal");
 
+
+    //OCULTAR BOTON PAGAR , si no hay prods en carro
+    function getCarrito() {
+        return JSON.parse(localStorage.getItem("carritoItems")) || [];
+    }
+    function mostrarBtnPago() {
+        let carrito = getCarrito();
+
+        if (carrito.length == 0) {
+            formPagar.classList.add("displayNone");
+        }
+    }
+
+    //MODAL CONFIRMACION PAGO (ABRIR/CERRAR)
     formPagar.addEventListener("submit", (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
         modal.classList.remove("displayNone");
     });
 
@@ -18,5 +32,5 @@ document.addEventListener("DOMContentLoaded", () => {
     btnCancelar.addEventListener("click", () => {
         modal.classList.add("displayNone");
     });
-
+    mostrarBtnPago();
 });
